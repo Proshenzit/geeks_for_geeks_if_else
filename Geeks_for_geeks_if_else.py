ad1db768ddcfb50b8b7e4a044e813e021f621447
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-08-20 04:02:31 -0700

    Add files via upload

---
+++ Geeks_for_geeks_if_else.py
@@ -0,0 +1,8 @@
+i = int (input("ENTER YOUR  VALUE OF I : "))
+if ( i < 15 ):
+    print("I is smaller than 15 ")
+    print(" i am in if block")
+else:
+
+    print(" i is greater than  15 ")
+    print(" i am in else block ")
